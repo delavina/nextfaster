ecfc93108e74c3ab5b2f6a0f9e690e740bb395b6
delavinamedia <koeppen.h@gmail.com>
2024-11-17 17:09:33 +0100

    update next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    ppr: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
